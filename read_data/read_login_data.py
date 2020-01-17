686f9a0641f1828d688a7d35489889c6155f9736
Jian Xiaohui <624767241@qq.com>
2020-01-17 14:48:46 +0800

    修改文件解析方法中的路径为绝对路径

--- read_data/read_login_data.py
+++ read_data/read_login_data.py
@@ -1,9 +1,17 @@
 import yaml
+
+from conftest import BASE_DIR
+
+
 def read_data():
-    with open('../data/login_test.yaml',encoding='utf-8')as f:
+    # with open('../data/login_test.yaml', encoding='utf-8')as f:
+    with open(BASE_DIR + '/data/login_test.yaml',encoding='utf-8')as f:
         data = yaml.safe_load(f)
         data_list = []
         for i in data:
             data_list.append((i.get('username'),i.get('pwd')))
         print(data_list)
         return data_list
+
+if __name__ == '__main__':
+    read_data()
